96d5365ebcc97c0d8a995b90cafcb0f4ec9d2608
Marcos Paulo <marcosp.mail@gmail.com>
2020-09-08 14:51:51 -0300

    Adds cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
 import 'express-async-errors'
 
 import uploadConfig from './config/upload'
@@ -9,6 +10,7 @@ import AppError from './errors/AppError'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use(routes)
